90d6d774ae801807ec6143e87e38ee178ce5fd17
Konrad Weihmann <kweihmann@outlook.com>
2019-07-27 16:51:22 +0200

    Add check on forward/reverse options
    Closes #9

--- systemdlint/systemdlint/cls/specials.py
+++ systemdlint/systemdlint/cls/specials.py
@@ -20,6 +20,32 @@ class SpecialTypeOneShotExecStart(object):
                         stash.append(UnitItem(file=i.File, line=i.Line, preerror=[ErrorMultiplicity("This type of unit can only have 1 ExecStart setting", i.Line, i.File)]))
         return stash
 
+class SpecialForwardReverseOption(object):
+    def Run(self, stash):
+        uniqunits = list(set([x.UnitName for x in stash]))
+        check_items = [
+                        ["Before", "After"],
+                        ["Requires", "RequiredBy"],
+                        ["Wants", "WantedBy"],
+                        ["PropagatesReloadTo", "ReloadPropagatedFrom"],
+                        ["ReloadPropagatedFrom", "PropagatesReloadTo"]
+                        ]
+        for u in uniqunits:
+            for c in check_items:
+                _na = [x for x in stash if x.Key == c[0] and x.UnitName == u]
+                _nb = [x for x in stash if x.Key == c[1] and x.UnitName == u]
+                _l = list(set([x.Value for x in _na]) & set([x.Value for x in _nb]))
+                for l in _l:
+                    _ia = next(obj for obj in _na if obj.Value == l)
+                    _ib = next(obj for obj in _nb if obj.Value == l)
+                    stash.append(UnitItem(file=_ia.File, line=_ia.Line, preerror=[
+                            ErrorConflictingOptions("'{}' and '{}' cannot be set to the same value of '{}'".format(c[0], c[1], l), _ia.Line, _ia.File)
+                        ]))
+                    stash.append(UnitItem(file=_ib.File, line=_ib.Line, preerror=[
+                            ErrorConflictingOptions("'{}' and '{}' cannot be set to the same value of '{}'".format(c[0], c[1], l), _ib.Line, _ib.File)
+                        ]))
+        return stash
+
 class SpecialRestartAlwaysConflicts(object):
     def Run(self, stash):
         restart_always = [x for x in stash if x.Key == "Restart" and x.Value == "always"]
@@ -87,7 +113,8 @@ class SpecialDepCycle(object):
         return self.__buildTree(stash)
 
 SPECIALS_SINGLEITEM = [
-    SpecialTypeOneShotExecStart()
+    SpecialTypeOneShotExecStart(),
+    SpecialForwardReverseOption()
 ]
 
 SPECIALS_ALLITEMS = [

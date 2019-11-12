f577a741c5a9da907282b58cf87da786ead5b33b
Konrad Weihmann <kweihmann@outlook.com>
2019-11-12 15:57:27 +0100

    Filter out dropins
    Closes #16

--- systemdlint/systemdlint/cls/runargs.py
+++ systemdlint/systemdlint/cls/runargs.py
@@ -7,6 +7,13 @@ RUNARGS = None
 
 DEFAULT_VERSION = "9.99999"
 
+def __isNoDropIn(_file):
+    name, ext = os.path.splitext(os.path.basename(_file))
+    parentdir = os.path.basename(os.path.dirname(_file))
+    if ext == ".conf" and parentdir.endswith(".d"):
+        return False
+    return True
+
 def ArgParser():
     global RUNARGS
     global DEFAULT_VERSION
@@ -19,7 +26,7 @@ def ArgParser():
     parser.add_argument("files", nargs='+', help="Files to parse")
     RUNARGS = parser.parse_args()
     ## Turn all paths to abs-paths right here
-    RUNARGS.files = [os.path.abspath(x) for x in RUNARGS.files]
+    RUNARGS.files = [os.path.abspath(x) for x in RUNARGS.files if __isNoDropIn(x)]
     if RUNARGS.sversion == DEFAULT_VERSION:
         ## Auto determine used systemd version, if not overriden from outside
         RUNARGS.sversion = Helper.GetSystemdVersion(RUNARGS.rootpath, DEFAULT_VERSION)

5918584a943a1e26ef64771a79ae3414494c37c8
Konrad Weihmann <kweihmann@outlook.com>
2019-12-01 18:36:43 +0100

    Release 1.1.5

--- systemdlint/setup.py
+++ systemdlint/setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="systemdlint",
-    version="1.1.4",
+    version="1.1.5",
     author="Konrad Weihmann",
     author_email="kweihmann@outlook.com",
     description="Systemd Unitfile Linter",

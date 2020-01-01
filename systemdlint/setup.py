57a6aa782254b4aaca4d6b1d493b6210cd1139ba
Konrad Weihmann <kweihmann@outlook.com>
2020-01-01 13:40:21 +0100

    Add requirements.txt to source release
    Update to release 1.1.6

--- systemdlint/setup.py
+++ systemdlint/setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="systemdlint",
-    version="1.1.5",
+    version="1.1.6",
     author="Konrad Weihmann",
     author_email="kweihmann@outlook.com",
     description="Systemd Unitfile Linter",

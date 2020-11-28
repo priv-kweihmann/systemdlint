c54a2c5545a78c3f47360adab9eea36cdc3f84c9
Konrad Weihmann <kweihmann@outlook.com>
2020-11-28 22:04:38 +0100

    Release 1.1.16
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- systemdlint/setup.py
+++ systemdlint/setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="systemdlint",
-    version="1.1.15",
+    version="1.1.16",
     author="Konrad Weihmann",
     author_email="kweihmann@outlook.com",
     description="Systemd Unitfile Linter",
